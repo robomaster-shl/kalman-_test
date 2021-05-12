384f48c761788877db24151fcd1351db490c6036
不叫耶稣的上帝 <57082321+robomaster-shl@users.noreply.github.com>
2021-05-12 16:53:38 +0800

    Update kalmantest.h

--- kalmantest/kalmantest.h
+++ kalmantest/kalmantest.h
@@ -17,10 +17,35 @@ public:
 private:
     cv::KalmanFilter KF_;
     Mat measurement_matrix;
-
+    int k=5;
     double t = 0.005;
     float x = CAMERA_RESOLUTION_COLS*0.5;
     float y = CAMERA_RESOLUTION_ROWS*0.5;
+};
+
+class text{
+    public:
+    text(int size){
+        data.resize(size);
+        index = 0;
+    }
+    ~text() = default;
+    void push(Point2f num){
+        data[index++] = num;
+        index %= data.size();
+    }
+    Point2f print(int x){
+        return data[x];
+    }
+    int re(){
+        length++;
+        return length;
+    }
+    private:
+        vector<Point2f> data;
+    int index;
+    int sum=0;
+    int length=0; 
 };
 
 #endif // RM_KALMANFILTER_H
